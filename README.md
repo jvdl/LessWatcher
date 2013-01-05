6e3bb260fe0eb770d4053ce6467cdb95b6918284
John V <john@afterlight.com.au>
2013-01-05 12:43:52 +1100

    Initial add of files

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-LessWatcher
-===========
+#LessWatcher
+LessWatcher is a globally installable shorthand for using less-monitor. 
+	
+	usage: lesswatcher [options]
+	
+	options:
+	  -h, --help              Print help (this message) and exit.
+	
+	  -d, --directory         Set input directory - where your .less files are located
+	
+	  -o, --output            Set output directory - this is relative to your the directory
+	                          where less files are generated from.
+	
+	  -b, --build-only        Performs a build and exits
+	
+	  --debug                 Outputs filename and line numbers in rendered CSS
+	
+	Example:
+	
+	  lesswatcher -d=styles/less -o=../css --debug
 
-An easy to use, globally installable .less compiler for NodeJS
+Alternative, a `lesswatcher.config` file can be placed in the directory where you are calling it from to specify a set of options.
+
+For example the following content in `lesswatcher.config` will set the corresponding options:
+
+	{
+		directory: "styles/less",
+		output: "../css",
+		debug: true
+	}
---
+++ lesswatcher.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var helper = require("./lib/helper"),
+	colors = require("colors"),
+	monitor = require("less-monitor");
+
+var configFile = "lesswatcher.config";
+
+//default options
+var options = {
+	directory: "styles/less/",
+	output: "../css/", //based on the dir where less files are parsed from
+	options: true,
+	master: true,
+	force: true,
+	debug: true,
+	debugType: "comment"
+};
+
+
+
+helper.readConfig(configFile, function(configFileOptions){
+	options = helper.extend(options, configFileOptions);
+	main();
+});
+
+
+function main() {
+
+	var args = process.argv.slice(1),
+		continueProcessing = true,
+		currentErrorcode;
+
+	args = args.filter(function (arg) {
+		var match;
+
+		if (match = arg.match(/^--?([a-z][0-9a-z-]*)(?:=([^\s]+))?$/i)) {
+			arg = match[1];
+		}
+		else {
+			return arg;
+		}
+
+		switch (arg) {
+			case "b":
+			case "build-only":
+				monitor.app.on("parseCompleteAll", function( filesMap, options) {
+					cli.logLine();
+					cli.log( "All files parsed and saved.".cyan );
+					cli.logLine();
+					cli.log("Build Complete - Exiting");
+					cli.logLine();
+
+					process.removeAllListeners("exit");
+					process.exit();
+				});
+				break;
+			case "d":
+			case "directory":
+				options.directory = match[2];
+				break;
+			case "o":
+			case "output":
+				options.output = match[2];
+				break;
+			case "h":
+			case "help":
+				require("./lib/usage").print();
+				continueProcessing = false;
+		}
+
+	});
+
+	if (!continueProcessing) {
+		return;
+	}
+
+	var cli = monitor.cli( monitor.app );
+
+	cli.config = helper.extend(cli.config, options);
+
+	monitor.app.init( options );
+
+}
